b2fca1396583a353183a5dc471c1412458d349de
베어 <bear@beeoui-MacBookPro.local>
2022-09-29 14:30:58 +0900

    firat commit

---
+++ lib/screens/list_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_book/models/book.dart';
+import 'package:flutter_book/repositories/book.repository.dart';
+import 'package:flutter_book/screens/detail_screen.dart';
+
+class ListScreen extends StatelessWidget {
+  final List<Book> books = BookRepository().getBooks();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('도서 목록 앱'),
+      ),
+      body: ListView.builder(
+        itemCount: books.length,
+        itemBuilder: (context, index) {
+          return BookTile(book: books[index]);
+        },
+      ),
+    );
+  }
+}
+
+class BookTile extends StatelessWidget {
+  final Book book;
+  BookTile({required this.book});
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(book.title),
+      leading: Image.network(book.image),
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => DetailScreen(
+            book: book,
+          ),
+        ));
+      },
+    );
+  }
+}
